9e56daf67a15caf9dba264bf15282cee6a226289
shiyaam <shiyaamsunderms@gmail.com>
2021-09-11 16:40:47 +0530

    export flex, grid

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { Badge } from "./components/badge/badge";
 export { Box } from "./components/box/box";
 export { Button } from "./components/button/button";
 export { ButtonGroup } from "./components/button/button-group";
+export { Flex } from "./components/flex/flex";
+export { Grid } from "./components/grid/grid";
 
 // Stitches
 export { styled, getCssText } from "./stitches.config";
